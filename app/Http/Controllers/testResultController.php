c3650bed1fa2a5fd9d35cdaa50e1f70c2ebb6c16
A-Div <small.ball.92@gmail.com>
2020-07-03 10:52:34 +0300

    add full report
    added full report

--- app/Http/Controllers/testResultController.php
+++ app/Http/Controllers/testResultController.php
@@ -116,6 +116,106 @@ class testResultController extends Controller
 
     public function full(Request $request)
     {
-        return view('testing.testResults.full');
+        $test = DB::table('tests')
+            ->leftJoin('type_test', 'type_test.id', '=', 'tests.type_id')
+            ->leftjoin('pers_tests', 'pers_tests.test_id', 'tests.id')
+            ->leftjoin('persons', 'persons.id', 'pers_tests.pers_id')
+            ->select(
+                'tests.*',
+                'type_test.name as typeTestName',
+                'pers_tests.start_time',
+                'pers_tests.timeLeft',
+                'pers_tests.end_time',
+                'persons.famil',
+                'persons.name',
+                'persons.otch',
+                'persons.pers_type'
+            )
+            ->where('pers_tests.id', $request->ptid)
+            ->first();
+        $countAllQuestion = $test->count_question;
+        $maxBall = $test->max_ball;
+        $test_name = $test->typeTestName . ' - ' . $test->discipline;
+        $fio = $test->famil . ' ' . $test->name . ' ' . $test->otch;
+
+        $countAnswer = DB::table('pers_test_details')
+            ->where('pers_test_id', $request->ptid)
+            ->whereNotNull('answer_id')
+            ->count();
+        $countTrueAnswer = DB::table('pers_test_details')
+            ->where('pers_test_id', $request->ptid)
+            ->where('answer_ball', ">", 0)
+            ->whereNotNull('answer_id')
+            ->count();
+
+        $countFalseAnswer = DB::table('pers_test_details')
+            ->where('pers_test_id', $request->ptid)
+            ->where('answer_ball', "=", 0)
+            ->whereNotNull('answer_id')
+            ->count();
+
+        $correctBall = DB::table('pers_test_details')
+            ->where('pers_test_id', $request->ptid)
+            ->whereNotNull('answer_id')
+            ->sum('answer_ball');
+
+        $fullTest = DB::table('pers_test_details as ptd')
+            ->where('ptd.pers_test_id', $request->ptid)
+            ->whereNotNull('ptd.answer_id')
+            ->get();
+        
+        $fullResult = [];
+        $quest_num = 0;
+        foreach ($fullTest as $ft)
+        {
+            $quest_num++;
+            if ($ft->answer_ball == 0)
+            {
+                $quest_text = DB::table('question_details')->where('id', $ft->quest_text_id)->first()->text;
+                $answer_text = DB::table('question_details')->where('id', $ft->answer_id)->first()->text; 
+                $cor_text = DB::table('question_details')->where('quest_id', $ft->quest_id)->where('type', 'cor')->first()->text;
+                $quest_ball = $ft->quest_ball;
+                $fullResult += [ 
+                    $quest_num => [
+                        'quest_num'     => $quest_num,
+                        'quest_text'    => $quest_text,
+                        'answer_text'   => $answer_text,
+                        'cor_text'      => $cor_text,
+                        'quest_ball'    => $quest_ball
+                    ]  
+                ];
+            }
+        }
+        
+        return view('testing.testResults.full',
+        [
+            'test_name'         => $test_name,
+            'countAllQuestion'  => $countAllQuestion,
+            'maxBall'           => $maxBall,
+            'countAnswer'       => $countAnswer,
+            'countTrueAnswer'   => $countTrueAnswer,
+            'countFalseAnswer'  => $countFalseAnswer,
+            'correctBall'       => $correctBall,
+            'fio'               => $fio,
+            'test_type'         => $test->typeTestName,
+            'fullResult'        => $fullResult
+        ]);
+
+        /*$pdf = PDF::loadView(
+            'testing.testResults.full',
+            [
+                'test_name'         => $test_name,
+                'countAllQuestion'  => $countAllQuestion,
+                'maxBall'           => $maxBall,
+                'countAnswer'       => $countAnswer,
+                'countTrueAnswer'   => $countTrueAnswer,
+                'countFalseAnswer'  => $countFalseAnswer,
+                'correctBall'       => $correctBall,
+                'fio'               => $fio,
+                'test_type'         => $test->typeTestName,
+                'fullResult'        => $fullResult
+            ]
+        );
+        return $pdf->stream();*/
     }
 }
