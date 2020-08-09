233a5a2ff43bf69e745c246000ca2e2f6d2b6ccf
A-Div <small.ball.92@gmail.com>
2020-08-09 22:53:37 +0300

    Update persController.php

--- app/Http/Controllers/persController.php
+++ app/Http/Controllers/persController.php
@@ -63,6 +63,15 @@ class persController extends Controller
                 }
             }
         }
+        $person = DB::table('persons')->where('id', '=' , session('pers_id'))->first();
+        $person_statements = DB::table('abit_statements')
+            ->leftjoin('abit_group as g', 'g.id', 'abit_statements.group_id')
+            ->leftjoin('abit_facultet as af', 'af.id', 'g.fk_id')
+            ->leftjoin('abit_formObuch as fo', 'fo.id', 'g.fo_id')
+            ->leftjoin('abit_stlevel as st', 'st.id', 'g.st_id')
+            ->where('abit_statements.person_id', $person->id)
+            ->select('af.branch_id', 'af.name as fac_name', 'g.name as spec_name', 'abit_statements.shifr_statement', 'fo.name as form_obuch', 'abit_statements.date_return', 'abit_statements.id', 'st.name as stlevel_name', 'abit_statements.is_original')
+            ->get();
 
         $persTests = DB::table('pers_tests')
             ->where('pers_tests.pers_id', session('pers_id'))
@@ -99,7 +108,7 @@ class persController extends Controller
             $testScatter_success = true;
             if (count($tc) == 0) $testScatter_success = false;
             
-            foreach ($tc as $tmp)
+            /*foreach ($tc as $tmp)
             {
                 $max_ball += $tmp->ball_count * $tmp->ball;
                 $max_quest += $tmp->ball_count;
@@ -150,7 +159,84 @@ class persController extends Controller
             }
             $statusTest += [
                 $test->id => $status
+            ];*/
+            
+            foreach ($tc as $tmp)
+            {
+                $max_ball += $tmp->ball_count * $tmp->ball;
+                $max_quest += $tmp->ball_count;
+
+                $ttmp = DB::table('questions')->where('test_id', $test->test_id)->where('ball', $tmp->ball)->count();
+                if ($tmp->ball_count <= $ttmp) $testScatter_success= true;
+                else $testScatter_success = false;
+            }
+            if ($max_ball != $test->max_ball) $testScatter_success = false;
+            if ($max_quest != $test->count_question) $testScatter_success = false;
+            // добавить проверку на дату и время ивента этого теста
+            $event = DB::table('pers_events')
+                        ->leftjoin('events', 'events.id', 'pers_events.event_id')
+                        ->where('pers_events.id', $test->pers_event_id)
+                        ->select('events.*')
+                        ->first();
+
+            //if((strtotime($event->date_start) <= time()) && (strtotime($event->date_end) >= time()) || $test->status == 2)
+            if((strtotime($test->start_time) <= time()) && (strtotime($event->date_end) >= time()) || $test->status == 2)
+            {
+                if ($test->start_time != null)
+                {
+                    /*$timestampStart = strtotime($test->start_time);
+                    $timestampEnd = time();
+                    $seconds = ($timestampEnd - $timestampStart);
+                    $testScatter_success = $seconds >= 172800 ? false : true; */// 172800 sec == 2 days
+                    $testScatter_success = true;
+                }
+                else $testScatter_success = false;
+            }
+            else $testScatter_success = false;
+
+            if ($testScatter_success)
+            {
+                switch ($test->status) {
+                    case 0:
+                        if ($person->is_home == 'T' || $request->ip == '195.189.44.155' || $person_statements->first()->branch_id == 2)
+                            $status = "<span style='cursor:default;background-color: forestgreen;' class='badge badge-primary'>Готов к прохождению</span>";
+                        else 
+                            $status = "<span class='badge badge-danger'>Тест не доступен</span>";
+                        break;
+                    case 1:
+                        if ($person->is_home == 'T' || $request->ip == '195.189.44.155' || $person_statements->first()->branch_id == 2)
+                            $status = "<span style='cursor:default;' class='badge badge-warning'>Продолжить</span>";
+                        else 
+                            $status = "<span class='badge badge-danger'>Тест не доступен</span>";
+                        break;
+                    case 2:
+                        $status = $test->test_ball_correct >= $test->min_ball ?
+                                    "<span style='cursor:default;' class='badge badge-success'>Пройден</span>" :
+                                    "<span style='cursor:default;' class='badge badge-danger'>Не пройден</span>";
+                        break;
+                    case 3:
+                        if ($person->is_home == 'T' || $request->ip == '195.189.44.155' || $person_statements->first()->branch_id == 2)
+                            $status = "<span style='cursor:default;' class='badge badge-warning'>Продолжить</span>";
+                        else 
+                            $status = "<span class='badge badge-danger'>Тест не доступен</span>";
+                        break;
+                }
+                if ($person->is_home == 'T' || $request->ip == '195.189.44.155' || $person_statements->first()->branch_id == 2)
+                    $successTest += [ $test->id => "true"];
+                else 
+                    $successTest += [ $test->id => "false"];
+            }
+            else
+            {
+                $status = "<span class='badge badge-danger'>Тест не доступен</span>";
+                $successTest += [
+                    $test->id => "false"
+                ];
+            }
+            $statusTest += [
+                $test->id => $status
             ];
+
         }
 
 
